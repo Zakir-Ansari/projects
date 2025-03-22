71bfc8b991fc09eb96c7d1f3422ebec732eac5c7
Zakir-Ansari <azakir780@gmail.com>
2025-03-22 16:56:39 +0530

    First commit of angular application

---
+++ pick-kart/frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NgbAccordionModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'pick-kart';
+}
