69f86785a91f631e22542f8d53fa8d5b996a7b7a
Zakir-Ansari <azakir780@gmail.com>
2025-03-22 17:10:44 +0530

    Adding prettier configurations

--- pick-kart/frontend/src/app/app.component.ts
+++ pick-kart/frontend/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NgbAccordionModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pick-kart';
